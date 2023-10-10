d99acd618f0f469d008009730fa985722c96b33e
victor-auffret <victor.auffret@orange.fr>
2023-10-10 11:39:22 +0200

    add doc command

--- dist/data-table.d.ts
+++ dist/data-table.d.ts
@@ -1,21 +1,31 @@
 import { FunctionComponent } from 'react';
-interface IColumn {
+/**
+ * @property title nom de la colonne à afficher
+ * @property data nom de la propriété du tableau data de IProps
+ * @interface
+ */
+export interface IColumn {
     title: string;
     data: string;
 }
-interface IProps {
+/**
+ * @property data - tableau de donnée avec des valeurs affichable sous forme de string
+ * @property columns - tableau de IColumn contenant le nom de la propriété et le titre de la colonne
+ * @property listNbPerPage - tableau contenant le nombre de page à afficher modifiable
+ * @interface
+ */
+export interface IProps {
     data: any[];
     columns: IColumn[];
     listNbPerPage: number[];
 }
 /**
  * DataTableComponent : Composant réact pour afficher un tableau de donnée avec un système de tri et de pagination
- * @param props
- *  - data : les données à afficher, paramètre sous forme de tableau d'objet
- *  - columns : un tableau contenant la liste des propriétés de data à afficher en tant que colonne du tableau
- *  - listNbPerPage : un paramètre optionnel qui est un tableau contenant les entiers symbolisant le nombre d'élément par page (par défaut on a 10, 25, 50 et 100 éléments par page au choix)
- * @returns Composant React DataTableComponent
+ * @param props - propriétés react,
+ * @param props.data - les données à afficher, paramètre sous forme de tableau d'objet,
+ * @param props.columns - un tableau contenant la liste des propriétés de data à afficher en tant que colonne du tableau,
+ * @param props.listNbPerPage - un paramètre obligatoire mais qui peut être vide pour utiliser les valeurs par défaut,
+ * @returns FunctionComponent<IProps> - Composant React DataTableComponent
  */
 declare const DataTableComponent: FunctionComponent<IProps>;
-export type { IColumn, IProps };
 export { DataTableComponent };
